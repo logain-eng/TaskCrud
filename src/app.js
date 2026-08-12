9f93a4c5daaf707476651f8cc180764a8c67bdb7
logain <logain0561@gmail.com>
2026-08-12 17:58:55 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 // Use Routers
 app.use(userRouter);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running...");
 });
+
